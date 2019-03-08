4165f389f58d7a42cfd22ac0e6c3b9e55a54098a
xizlt <ivantreada@gmail.com>
2019-03-08 12:02:05 +0300

    login

--- login.php
+++ login.php
@@ -17,12 +17,52 @@ $categories = getCategories($connection);
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $form = $_POST;
+    $login_data = $_POST;
 
-    $user = check_user($connection, $form);
+
+    function validate_login($connection, $login_data){
+        if ($error = validate_password($login_data['password'])) {
+            $errors['password'] = $error;
+        }
+        if ($error = validate_email($login_data['email'], $connection)) {
+            $errors['email'] = $error;
+        }
+    }
+
+    function validate_password($password)
+    {
+        if (empty($password)) {
+            return 'Заполните поле пароль';
+        }
+        if (mb_strlen($password) > 255){
+            return 'Допустимая длина строки 255 символов';
+        }
+        return null;
+    }
+
+    function validate_email($email, $connection)
+    {
+        if (empty($email)) {
+            return 'Заполните поле email';
+        }
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return 'Проверьте правильность написания email';
+        }
+        if (mb_strlen($email) > 320) {
+            return 'Допустимая длина email 320 символов';
+        }
+        return null;
+    }
+
+    $errors = validate_login($connection, $login_data);
+
+
+
+/*
+    $user = check_user($connection, $login_data);
 
     if (!count($errors) and $user) {
-        if (password_verify($form['password'], $user['password'])) {
+        if (password_verify($login_data['password'], $user['password'])) {
             $_SESSION['user'] = $user;
         } else {
             $errors['password'] = 'Неверный пароль';
@@ -32,17 +72,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     if (count($errors)) {
-        $page_content = include_template('login.php', ['form' => $form, 'errors' => $errors]);
+        $page_content = include_template('login.php', ['login_data' => $login_data, 'errors' => $errors]);
     } else {
         header("Location: /index.php");
         exit();
     }
 }
-
+*/
 $page_content = include_template('login.php', [
     'categories' => $categories,
     'errors' => $errors,
-    'form' => $form
+    'login_data' => $login_data
 ]);
 
 $layout = include_template('layout.php', [
