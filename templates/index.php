a3949a16d1f7558998ed677cd84e2b3881299f02
xizlt <ivantreada@gmail.com>
2019-03-11 17:19:29 +0300

    вот теперь все

--- templates/index.php
+++ templates/index.php
@@ -27,15 +27,15 @@
                         <img src="<?= $lot['img']; ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?= get_value($category, 'name'); ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="lot.php?id=<?= $lot['id']; ?>"><?= get_value($lot, 'name'); ?></a></h3>
+                        <span class="lot__category"><?= clean(get_value($category, 'name')); ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="lot.php?id=<?= $lot['id']; ?>"><?= clean(get_value($lot, 'name')); ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?= formatPrice(get_value($lot, 'start_price')); ?></span>
+                                <span class="lot__cost"><?= formatPrice(clean(get_value($lot, 'start_price'))); ?></span>
                             </div>
                             <div class="lot__timer timer">
-                                <?= time_before_end(get_value($lot, 'end_time')) ?>
+                                <?= time_before_end(clean(get_value($lot, 'end_time'))) ?>
                             </div>
                         </div>
                     </div>
