c1be17a68c2ed13f6a610be7085d9bc1eee32e80
xizlt <ivantreada@gmail.com>
2019-03-11 18:12:55 +0300

    доп функция

--- functions/upload.php
+++ functions/upload.php
@@ -9,6 +9,8 @@ function upload_img($file_data)
     $path = get_value($file_data, 'name');
     $tmp_name = get_value($file_data, 'tmp_name');
     $result = 'uploads/' . $path;
-    move_uploaded_file($tmp_name, $result);
+    if (!move_uploaded_file($tmp_name, $result)) {
+        die('Не найдена папка uploads или отсутствуют права на запись в неё');
+    }
     return $result;
 }
