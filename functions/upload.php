c0827c01285487fedafc7a256af3b927b8acc370
xizlt <ivantreada@gmail.com>
2019-03-11 19:19:25 +0300

    маленькая правка

--- functions/upload.php
+++ functions/upload.php
@@ -7,6 +7,9 @@
 function upload_img($file_data)
 {
     $path = get_value($file_data, 'name');
+    if (!$path){
+        return null;
+    }
     $tmp_name = get_value($file_data, 'tmp_name');
     $result = 'uploads/' . $path;
     if (!move_uploaded_file($tmp_name, $result)) {
